3ceba53d12c720c385c9ce9525c69c7ef0cb62a9
Santiago Esplugas <santiago_esplugas@live.com.ar>
2021-06-29 23:18:40 -0300

    cambio bueno

--- Prueba001/WebApplication2/Startup.cs
+++ Prueba001/WebApplication2/Startup.cs
@@ -27,8 +27,6 @@ namespace WebApplication2
         {
             services.AddControllersWithViews();     
             services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
